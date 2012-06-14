819c88b0647174aa604fa49f8a8760db28269e00
Nick Smallbone <nick.smallbone@gmail.com>
2012-06-14 22:20:00 +0200

    Tweak the functions example.

--- Example.hs
+++ Example.hs
@@ -35,8 +35,13 @@ lists _ = [
 funs :: forall a. (Typeable a, Ord a, Arbitrary a, CoArbitrary a) => a -> [Sig]
 funs ty = [
   vars ["f", "g", "h"] (undefined :: a -> a),
+  -- We treat . as a function of two arguments here (blind2)---i.e.,
+  -- we do not generate terms of the form (f . g) x.
   blind2 "." (\(f :: a -> a) (g :: a -> a) -> f . g),
+  -- Similarly, id is not treated as a function.
   blind0 "id" (id :: a -> a),
+  -- Tell QuickSpec how to compare values of function type:
+  -- i.e., generate a random argument and apply the function to it.
   observer1 (\(x :: a) (f :: a -> a) -> f x)
   ]
 
