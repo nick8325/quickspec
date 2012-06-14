ba41f2ade7bb341254e101aae5af4907878028bf
Nick Smallbone <nick.smallbone@gmail.com>
2012-06-14 21:28:01 +0200

    Remove test cases from TestTree.

--- TestTree.hs
+++ TestTree.hs
@@ -14,39 +14,40 @@ import Control.Parallel.Strategies
 -- refinement of t (it may be trivial, so that length (branches t) == 1).
 -- As a special case, a TestTree may be Nil, but Nil may not appear in
 -- the branches of a TestTree.
-data TestTree r a = Nil | NonNil (TestTree' r a)
-data TestTree' r a = Tree { rep :: a, rest :: [a], eval :: a -> r, branches :: [TestTree' r a] }
+data TestTree a = Nil | NonNil (TestTree' a)
+data TestTree' a = Tree { rep :: a, rest :: [a], branches :: [TestTree' a] }
 
 -- Precondition: bs must be sorted according to the TestCase.
-tree :: Ord r => [a] -> (a -> r) -> [TestTree' r a] -> TestTree' r a
+tree :: Ord r => [a] -> (a -> r) -> [TestTree' a] -> TestTree' a
 tree [] _ _ = error "TestTree.tree: bug: empty equivalence class"
 tree (x:xs) eval bs =
   assert (isSortedBy (eval . rep) bs) $
-    Tree { rep = x, rest = xs, eval = eval, branches = bs }
+    Tree { rep = x, rest = xs, branches = bs }
 
-terms :: TestTree r a -> [a]
+terms :: TestTree a -> [a]
 terms Nil = []
 terms (NonNil t) = terms' t
 
-terms' :: TestTree' r a -> [a]
+terms' :: TestTree' a -> [a]
 terms' Tree{rep = x, rest = xs} = x:xs
 
--- Precondition: the two test trees must have an identical sequence of test cases.
-union :: Ord r => TestTree r a -> TestTree r a -> TestTree r a
-Nil `union` t = t
-t `union` Nil = t
-NonNil t1 `union` NonNil t2 = NonNil (t1 `union'` t2)
+-- Precondition: the sequence of test cases given must be
+-- that used to generate the two TestTrees.
+union :: Ord r => [a -> r] -> TestTree a -> TestTree a -> TestTree a
+union _ Nil t = t
+union _ t Nil = t
+union evals (NonNil t1) (NonNil t2) = NonNil (union' evals t1 t2)
 
-union' :: Ord r => TestTree' r a -> TestTree' r a -> TestTree' r a
-t1 `union'` t2 =
-  tree (terms' t1 ++ terms' t2) (eval t1)
-         (merge union' (eval t1 . rep) (branches t1) (branches t2))
+union' :: Ord r => [a -> r] -> TestTree' a -> TestTree' a -> TestTree' a
+union' (eval:evals) t1 t2 =
+  tree (terms' t1 ++ terms' t2) eval
+         (merge (union' evals) (eval . rep) (branches t1) (branches t2))
 
-test :: Ord r => [a -> r] -> [a] -> TestTree r a
+test :: Ord r => [a -> r] -> [a] -> TestTree a
 test _ [] = Nil
 test tcs xs = NonNil (test' tcs xs)
 
-test' :: Ord r => [a -> r] -> [a] -> TestTree' r a
+test' :: Ord r => [a -> r] -> [a] -> TestTree' a
 test' [] _ = error "bug: ran out of test cases"
 test' (tc:tcs) xs = tree xs tc (map (test' tcs) bs)
   where bs = partitionBy tc xs
@@ -54,9 +55,9 @@ test' (tc:tcs) xs = tree xs tc (map (test' tcs) bs)
 -- A TestTree with finite depth, represented as a TestTree where some
 -- nodes have no branches. Since this breaks one of the TestTree
 -- invariants we use a different type.
-data TestResults a = forall r. Results (TestTree r a)
+newtype TestResults a = Results (TestTree a)
 
-cutOff :: Int -> TestTree r a -> TestResults a
+cutOff :: Int -> TestTree a -> TestResults a
 cutOff _ Nil = Results Nil
 cutOff n (NonNil t) = Results (NonNil (aux n t))
   where aux 0 t = t { branches = [] }
