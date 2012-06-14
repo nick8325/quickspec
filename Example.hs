ebe2d7e96cec1872edca034749793501bf524a56
Nick Smallbone <nick.smallbone@gmail.com>
2012-06-14 20:42:07 +0200

    Added more examples.

--- Example.hs
+++ Example.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import QuickSpec
 import Signature
+import Data.Monoid
+import Test.QuickCheck
+import Data.Typeable
 
 bools = [
   vars ["x", "y", "z"] True,
@@ -9,7 +14,36 @@ bools = [
   fun0 "True" True,
   fun0 "False" False]
 
-main = quickSpec bools
-  -- r <- fmap toList (generate bools 3)
-  -- forM_ r $ \(Some (C t)) ->
-  --   mapM_ print (classes t)
+arith :: forall a. (Typeable a, Ord a, Num a, Arbitrary a) => a -> [Sig]
+arith _ = [
+  vars ["x", "y", "z"] (undefined :: a),
+  fun0 "0" (0 :: a),
+  fun0 "1" (1 :: a),
+  fun2 "+" ((+) :: a -> a -> a),
+  fun2 "*" ((*) :: a -> a -> a)]
+
+lists :: forall a. (Typeable a, Ord a, Arbitrary a) => a -> [Sig]
+lists _ = [
+  vars ["xs", "ys", "zs"] (undefined :: [a]),
+  fun1 "length" (length :: [a] -> Int),
+  fun1 "reverse" (reverse :: [a] -> [a]),
+  fun2 "++" ((++) :: [a] -> [a] -> [a]),
+  fun0 "[]" ([] :: [a]),
+  fun2 "map" (map :: (a -> a) -> [a] -> [a]),
+  fun1 "unit" (return :: a -> [a])]
+
+funs :: forall a. (Typeable a, Ord a, Arbitrary a, CoArbitrary a) => a -> [Sig]
+funs ty = [
+  vars ["f", "g", "h"] (undefined :: a -> a),
+  blind2 "." (\(f :: a -> a) (g :: a -> a) -> f . g),
+  blind0 "id" (id :: a -> a),
+  observer2 (\(x :: a) (f :: a -> a) -> f x)
+  ]
+
+main = mapM_ quickSpec $ [
+  bools,
+  arith (undefined :: Int),
+  funs (undefined :: Int),
+  [silence (funs 'A'),
+   silence (arith (undefined :: Int)),
+   signature (lists 'A')]]
