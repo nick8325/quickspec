5a48f70bb252994c01e54f0c466664ba9bfbd36b
Nick Smallbone <nick.smallbone@gmail.com>
2012-06-17 00:54:34 +0200

    Split up example into several files.

---
+++ examples/Bools.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Test.QuickSpec hiding (bools)
+
+bools = [
+  ["x", "y", "z"] `vars` (undefined :: Bool),
+
+  "||"    `fun2` (||),
+  "&&"    `fun2` (&&),
+  "not"   `fun1` not,
+  "True"  `fun0` True,
+  "False" `fun0` False]
+
+main = quickSpec bools
---
+++ examples/Lists.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Test.QuickSpec hiding (lists)
+import Test.QuickCheck
+import Data.Typeable
+
+lists :: forall a. (Typeable a, Ord a, Arbitrary a, CoArbitrary a) =>
+         a -> [Sig]
+lists a = [
+  arith (undefined :: Int),
+  funs (undefined :: a),
+
+  ["xs", "ys", "zs"] `vars` (undefined :: [a]),
+
+  "[]"      `fun0` ([]      :: [a]),
+  ":"       `fun2` ((:)     :: a -> [a] -> [a]),
+  "unit"    `fun1` (return  :: a -> [a]),
+  "++"      `fun2` ((++)    :: [a] -> [a] -> [a]),
+  "length"  `fun1` (length  :: [a] -> Int),
+  "reverse" `fun1` (reverse :: [a] -> [a]),
+  "map"     `fun2` (map     :: (a -> a) -> [a] -> [a])
+  ]
+
+main = quickSpec (lists (undefined :: A))
