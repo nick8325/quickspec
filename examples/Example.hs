94fdff79ada30523dfe26f76d08a785f35acaf09
Nick Smallbone <nick.smallbone@gmail.com>
2012-06-17 00:47:23 +0200

    Tidied up example.

--- examples/Example.hs
+++ examples/Example.hs
@@ -6,34 +6,34 @@ import Test.QuickCheck
 import Data.Typeable
 
 bools = [
-  vars ["x", "y", "z"] (undefined :: Bool),
+  ["x", "y", "z"] `vars` (undefined :: Bool),
 
-  fun2 "||"    (||),
-  fun2 "&&"    (&&),
-  fun1 "not"   not,
-  fun0 "True"  True,
-  fun0 "False" False]
+  "||"    `fun2` (||),
+  "&&"    `fun2` (&&),
+  "not"   `fun1` not,
+  "True"  `fun0` True,
+  "False" `fun0` False]
 
 arith :: forall a. (Typeable a, Ord a, Num a, Arbitrary a) => a -> [Sig]
 arith _ = [
-  vars ["x", "y", "z"] (undefined :: a),
+  ["x", "y", "z"] `vars` (undefined :: a),
 
-  fun0 "0" (0   :: a),
-  fun0 "1" (1   :: a),
-  fun2 "+" ((+) :: a -> a -> a),
-  fun2 "*" ((*) :: a -> a -> a)]
+  "0" `fun0` (0   :: a),
+  "1" `fun0` (1   :: a),
+  "+" `fun2` ((+) :: a -> a -> a),
+  "*" `fun2` ((*) :: a -> a -> a)]
 
 lists :: forall a. (Typeable a, Ord a, Arbitrary a) => a -> [Sig]
 lists _ = [
-  vars ["xs", "ys", "zs"] (undefined :: [a]),
+  ["xs", "ys", "zs"] `vars` (undefined :: [a]),
 
-  fun0 "[]"      ([]      :: [a]),
-  fun2 ":"       ((:)     :: a -> [a] -> [a]),
-  fun1 "unit"    (return  :: a -> [a]),
-  fun2 "++"      ((++)    :: [a] -> [a] -> [a]),
-  fun1 "length"  (length  :: [a] -> Int),
-  fun1 "reverse" (reverse :: [a] -> [a]),
-  fun2 "map"     (map     :: (a -> a) -> [a] -> [a])
+  "[]"      `fun0` ([]      :: [a]),
+  ":"       `fun2` ((:)     :: a -> [a] -> [a]),
+  "unit"    `fun1` (return  :: a -> [a]),
+  "++"      `fun2` ((++)    :: [a] -> [a] -> [a]),
+  "length"  `fun1` (length  :: [a] -> Int),
+  "reverse" `fun1` (reverse :: [a] -> [a]),
+  "map"     `fun2` (map     :: (a -> a) -> [a] -> [a])
   ]
 
 funs :: forall a. (Typeable a, Ord a, Arbitrary a, CoArbitrary a) => a -> [Sig]
