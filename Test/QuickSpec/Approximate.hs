20c094d3ec5e968ed59b18e2102f9e8289dc2658
Nick Smallbone <nick.smallbone@gmail.com>
2013-02-13 15:37:59 +0100

    Binary search-based approximation (can't work)

--- Test/QuickSpec/Approximate.hs
+++ Test/QuickSpec/Approximate.hs
@@ -1,17 +1,17 @@
 -- Utilities for testing functions that return partial results.
-{-# LANGUAGE DeriveDataTypeable, StandaloneDeriving #-}
+{-# LANGUAGE DeriveDataTypeable, StandaloneDeriving, ScopedTypeVariables #-}
 module Test.QuickSpec.Approximate where
 
 import Test.QuickCheck
 import Test.QuickCheck.Modifiers
 import Test.QuickSpec.Signature
-import Data.List
 import Data.Typeable
+import Control.Spoon
 
 deriving instance Typeable1 NonNegative
 
 class (Typeable a, Ord a) => Approximate a where
-  approximate :: NonNegative Int -> a -> a
+  approximate :: Int -> a -> a
   approximate _ = id
 
 instance Approximate () where
@@ -20,16 +20,37 @@ instance Approximate Integer where
 instance Approximate Bool where
 
 instance Approximate a => Approximate [a] where
-  approximate n = genericTake n . map (approximate n)
+  approximate n = take n . map (approximate n)
 
-approximant :: Approximate a => a -> NonNegative Int -> a -> a
-approximant _ = approximate
+newtype Approximants a = Approximants a deriving Typeable
+
+instance Approximate a => Eq (Approximants a) where
+  x == y = x `compare` y == EQ
+
+instance Approximate a => Ord (Approximants a) where
+  compare x y = eval n x `compare` eval n y
+    where
+      eval n (Approximants x) = teaspoon ((y == y) `seq` y)
+        where y = approximate n x
+
+      n = search 0 100
+
+      -- binary search from m inclusive to n exclusive
+      -- important property: if eval n x = Nothing then eval (n+1) x = Nothing
+      search m n
+        | m > n = m
+        | otherwise =
+          case (eval mid x, eval mid y) of
+            (Nothing, Nothing) -> search m mid
+            (Just x', Just y') | x' == y' -> search (mid+1) n
+            _ -> mid
+        where mid = (m + n) `div` 2
 
 -- Wrapper functions around 'observer'.
 
-approximate1 :: Approximate a => a -> Sig
-approximate1 x = observer2 (approximant x)
+approximate1 :: forall a. Approximate a => a -> Sig
+approximate1 _ = observer1 (Approximants :: a -> Approximants a)
 
-approximate2 :: (Arbitrary a, Typeable a, Approximate b) =>
+approximate2 :: forall a b. (Arbitrary a, Typeable a, Approximate b) =>
                 (a -> b) -> Sig
-approximate2 f = observer3 (approximant . f)
+approximate2 f = observer1 (Approximants . f)
