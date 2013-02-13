d1d7711260d35cc0797c8ff618a7830b053176d2
Nick Smallbone <nick.smallbone@gmail.com>
2013-02-13 16:15:35 +0100

    A new applicative-based approximation module.

--- Test/QuickSpec/Approximate.hs
+++ Test/QuickSpec/Approximate.hs
@@ -1,56 +1,49 @@
 -- Utilities for testing functions that return partial results.
-{-# LANGUAGE DeriveDataTypeable, StandaloneDeriving, ScopedTypeVariables #-}
+{-# LANGUAGE DeriveDataTypeable, ExistentialQuantification, DefaultSignatures #-}
 module Test.QuickSpec.Approximate where
 
 import Test.QuickCheck
-import Test.QuickCheck.Modifiers
 import Test.QuickSpec.Signature
 import Data.Typeable
 import Control.Spoon
+import Data.Default
+import Control.Applicative hiding (Const)
 
-deriving instance Typeable1 NonNegative
+data Schema a
+  = Const a
+  | forall b. App (Schema (b -> a)) (Schema b)
+  | Plug a (Schema a)
 
-class (Typeable a, Ord a) => Approximate a where
-  approximate :: Int -> a -> a
-  approximate _ = id
+instance Functor Schema where
+  fmap f x = pure f <*> x
 
-instance Approximate () where
-instance Approximate Int where
-instance Approximate Integer where
-instance Approximate Bool where
+instance Applicative Schema where
+  pure = Const
+  (<*>) = App
 
-instance Approximate a => Approximate [a] where
-  approximate n = take n . map (approximate n)
+class Partial a where
+  lifted :: a -> Schema a
 
-newtype Approximants a = Approximants a deriving Typeable
+  default lifted :: Default a => a -> Schema a
+  lifted x = Plug def (unlifted x)
 
-instance Approximate a => Eq (Approximants a) where
-  x == y = x `compare` y == EQ
+  unlifted :: a -> Schema a
+  unlifted x = Const x
 
-instance Approximate a => Ord (Approximants a) where
-  compare x y = eval n x `compare` eval n y
-    where
-      eval n (Approximants x) = teaspoon ((y == y) `seq` y)
-        where y = approximate n x
+instance Partial () where
+instance Partial Int where
+instance Partial Integer where
+instance Default Bool where def = False
+instance Partial Bool where
 
-      n = search 0 100
+instance Partial a => Partial [a] where
+  unlifted [] = pure []
+  unlifted (x:xs) = (:) <$> lifted x <*> lifted xs
 
-      -- binary search from m inclusive to n exclusive
-      -- important property: if eval n x = Nothing then eval (n+1) x = Nothing
-      search m n
-        | m > n = m
-        | otherwise =
-          case (eval mid x, eval mid y) of
-            (Nothing, Nothing) -> search m mid
-            (Just x', Just y') | x' == y' -> search (mid+1) n
-            _ -> mid
-        where mid = (m + n) `div` 2
+partial :: Schema a -> Gen a
+partial (Const x) = return x
+partial (App f x) = partial f <*> partial x
+partial (Plug _ x) = frequency [(1, undefined), (3, partial x)]
 
--- Wrapper functions around 'observer'.
-
-approximate1 :: forall a. Approximate a => a -> Sig
-approximate1 _ = observer1 (Approximants :: a -> Approximants a)
-
-approximate2 :: forall a b. (Arbitrary a, Typeable a, Approximate b) =>
-                (a -> b) -> Sig
-approximate2 f = observer1 (Approximants . f)
+pvars :: (Arbitrary a, Typeable a, Partial a) => [String] -> a -> Sig
+pvars xs w = gvars xs $ (arbitrary `asTypeOf` return w) >>= partial . lifted
