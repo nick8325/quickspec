c8d71bfd45999f933672944986be6e5a51ac1712
Nick Smallbone <nick.smallbone@gmail.com>
2013-02-14 05:25:33 +0100

    Much more bearable implementation of approximation.

--- Test/QuickSpec/Approximate.hs
+++ Test/QuickSpec/Approximate.hs
@@ -1,5 +1,5 @@
 -- Utilities for testing functions that return partial results.
-{-# LANGUAGE DeriveDataTypeable, ExistentialQuantification, DefaultSignatures #-}
+{-# LANGUAGE Rank2Types #-}
 module Test.QuickSpec.Approximate where
 
 import Test.QuickCheck
@@ -7,37 +7,26 @@ import Test.QuickCheck.Gen
 import Test.QuickSpec.Signature
 import Test.QuickSpec.Utils
 import Test.QuickSpec.Utils.Typeable
+import Control.Monad
+import Control.Monad.Reader
 import Control.Spoon
-import Control.Applicative hiding (Const)
 import System.Random
 import Data.Monoid
 
-data Schema a
-  = Const a
-  | forall b. App (Schema (b -> a)) (Schema b)
-  | Plug (Either (Gen a) (Schema a))
+newtype Plug = Plug { unPlug :: forall a. Partial a => a -> Gen a }
+type GP = ReaderT Plug Gen
 
-instance Functor Schema where
-  fmap f x = pure f <*> x
-
-instance Applicative Schema where
-  pure = Const
-  (<*>) = App
+plug :: Partial a => a -> GP a
+plug x = do
+  f <- ask
+  ReaderT (\_ -> unPlug f x)
 
 class (Typeable a, Arbitrary a, Eq a) => Partial a where
-  unlifted :: a -> Schema a
-  unlifted x = Const x
+  unlifted :: a -> GP a
+  unlifted x = return x
 
-lifted :: Partial a => a -> Schema a
-lifted x = Plug $
-  case soupspoon (unlifted x) of
-    Nothing -> Left (coarbitraryShow (typeOf x) arbitrary) 
-    Just s -> Right s
-  where
-    soupspoon x =
-      case teaspoon x of
-        Just (Const y) -> fmap Const (spoony y)
-        y -> y
+lifted :: Partial a => a -> GP a
+lifted x = unlifted x >>= plug
 
 instance Partial ()
 instance Partial Int
@@ -45,30 +34,26 @@ instance Partial Integer
 instance Partial Bool
 
 instance Partial a => Partial [a] where
-  unlifted [] = pure []
-  unlifted (x:xs) = (:) <$> lifted x <*> lifted xs
+  unlifted [] = return []
+  unlifted (x:xs) = liftM2 (:) (lifted x) (lifted xs)
 
 approximate :: Partial a => (StdGen, Int) -> a -> a
-approximate (g, n) x = aux (lifted x)
+approximate (g, n) x = unGen (runReaderT (lifted x) (Plug plug)) g n
   where
-    aux :: Schema a -> a
-    aux (Const x) = x
-    aux (App f x) = aux f (aux x)
-    aux (Plug (Left gen)) = unGen gen g n
-    aux (Plug (Right s)) = aux s
+    plug :: forall a. Partial a => a -> Gen a
+    plug x =
+      case spoony x of
+        Nothing -> return (unGen arbitrary g n)
+        Just y -> return y
 
 pobserver :: (Ord a, Partial a) => a -> Sig
 pobserver x = observerSig (Observer (MkGen f))
   where f g n y = approximate (g, n `max` 50) (y `asTypeOf` x)
 
 genPartial :: Partial a => a -> Gen a
-genPartial = aux . lifted
+genPartial x = runReaderT (lifted x) (Plug plug)
   where
-    aux :: Schema a -> Gen a
-    aux (Const x) = return x
-    aux (App f x) = aux f <*> aux x
-    aux (Plug (Left _)) = undefined
-    aux (Plug (Right x)) = frequency [(1, undefined), (3, aux x)]
+    plug x = frequency [(1, undefined), (3, return x)]
 
 pvars :: (Ord a, Partial a) => [String] -> a -> Sig
 pvars xs w = 
