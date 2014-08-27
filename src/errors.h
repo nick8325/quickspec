3276b46abc80d8edc996fd4a6604da29e450af2e
Nick Smallbone <nick.smallbone@gmail.com>
2014-08-27 16:17:41 +0200

    Get rid of confusing type errors when using ERROR

--- src/errors.h
+++ src/errors.h
@@ -1,3 +1,3 @@
 -- Inspired by Agda's undefined.h
 #define __ (ERROR "no error message given")
-#define ERROR (\msg -> error ("Error at file " ++ __FILE__ ++ ", line " ++ show __LINE__ ++ ": " ++ msg))
+#define ERROR (\msg -> error ("Error at file " ++ __FILE__ ++ ", line " ++ show (__LINE__ :: Int) ++ ": " ++ msg))
