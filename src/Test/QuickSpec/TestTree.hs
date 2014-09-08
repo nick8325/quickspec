8c1c275a24f7a32524210c0fe4b96b0698b0e365
Nick Smallbone <nick.smallbone@gmail.com>
2014-09-08 23:41:25 +0200

    Get rid of special case for empty trees.

--- src/Test/QuickSpec/TestTree.hs
+++ src/Test/QuickSpec/TestTree.hs
@@ -16,48 +16,32 @@ import Control.Exception(assert)
 --
 -- A TestTree is always infinite, and branches t is always a
 -- refinement of t (it may be trivial, so that length (branches t) == 1).
--- As a special case, a TestTree may be Nil, but Nil may not appear in
--- the branches of a TestTree.
-data TestTree a = Nil | NonNil (TestTree' a)
-data TestTree' a = Tree { rep :: a, rest :: [a], branches :: [TestTree' a] }
+data TestTree a = Tree { rep :: a, rest :: [a], branches :: [TestTree a] }
 
 -- Precondition: bs must be sorted according to the TestCase.
-tree :: Ord r => [a] -> (a -> r) -> [TestTree' a] -> TestTree' a
+tree :: Ord r => [a] -> (a -> r) -> [TestTree a] -> TestTree a
 tree [] _ _ = ERROR "empty equivalence class"
 tree (x:xs) eval bs =
   assert (isSortedBy (eval . rep) bs) $
     Tree { rep = x, rest = xs, branches = bs }
 
 terms :: TestTree a -> [a]
-terms Nil = []
-terms (NonNil t) = terms' t
-
-terms' :: TestTree' a -> [a]
-terms' Tree{rep = x, rest = xs} = x:xs
+terms Tree{rep = x, rest = xs} = x:xs
 
 -- Precondition: the sequence of test cases given must be
 -- that used to generate the two TestTrees.
 union :: Ord r => [a -> r] -> TestTree a -> TestTree a -> TestTree a
-union _ Nil t = t
-union _ t Nil = t
-union evals (NonNil t1) (NonNil t2) = NonNil (union' evals t1 t2)
-
-union' :: Ord r => [a -> r] -> TestTree' a -> TestTree' a -> TestTree' a
-union' (eval:evals) t1 t2 =
-  tree (terms' t1 ++ terms' t2) eval
-         (merge (union' evals) (eval . rep) (branches t1) (branches t2))
+union (eval:evals) t1 t2 =
+  tree (terms t1 ++ terms t2) eval
+         (merge (union evals) (eval . rep) (branches t1) (branches t2))
 
 test :: Ord r => [a -> r] -> [a] -> TestTree a
-test _ [] = Nil
-test tcs xs = NonNil (test' tcs xs)
-
-test' :: Ord r => [a -> r] -> [a] -> TestTree' a
-test' [] _ =
-  error "Test.QuickSpec.TestTree.test': ran out of test cases"
-test' (tc:tcs) [] =
-  error "Test.QuickSpec.TestTree.test': found an empty equivalence class"
-test' (tc:tcs) xs@[_] = tree xs tc [test' tcs xs]
-test' (tc:tcs) xs = tree xs tc (map (test' tcs) bs)
+test [] _ =
+  error "Test.QuickSpec.TestTree.test: ran out of test cases"
+test (tc:tcs) [] =
+  error "Test.QuickSpec.TestTree.test: found an empty equivalence class"
+test (tc:tcs) xs@[_] = tree xs tc [test tcs xs]
+test (tc:tcs) xs = tree xs tc (map (test tcs) bs)
   where bs = partitionBy tc xs
 
 -- A TestTree with finite depth, represented as a TestTree where some
@@ -68,14 +52,13 @@ newtype TestResults a = Results (TestTree a)
 discrete :: Ord a => [a] -> TestResults a
 discrete xs =
   case sort xs of
-    [] -> Results Nil
-    (y:ys) ->
-      Results (NonNil (Tree y ys (map singleton (y:ys))))
+    [] -> error "Test.QuickSpec.TestTree.discrete: empty list"
+    y:ys ->
+      Results (Tree y ys (map singleton (y:ys)))
       where singleton x = Tree x [] []
 
 cutOff :: Int -> Int -> TestTree a -> TestResults a
-cutOff _ _ Nil = Results Nil
-cutOff m n (NonNil t) = Results (NonNil (aux m t))
+cutOff m n t = Results (aux m t)
   where aux _ t@Tree{rest = []} = t { branches = [] }
         aux 0 t = aux' False n n t
         aux m t = t { branches = map (aux (m-1)) (branches t) }
@@ -87,14 +70,12 @@ cutOff m n (NonNil t) = Results (NonNil (aux m t))
         aux' _ m n t = t { branches = map (aux' True (m-1) n) (branches t) }
 
 numTests :: TestResults a -> Int
-numTests (Results Nil) = 0
-numTests (Results (NonNil t)) = aux t
+numTests (Results t) = aux t
   where aux Tree{branches = []} = 0
         aux Tree{branches = bs} = 1 + maximum (map aux bs)
 
 numResults :: TestResults a -> Int
-numResults (Results Nil) = 0
-numResults (Results (NonNil t)) = aux t
+numResults (Results t) = aux t
   where aux Tree{rest = []} = 0
         aux Tree{rest = xs, branches = ts} =
           1 + length xs + sum (map aux ts)
@@ -103,8 +84,7 @@ classes :: Ord a => TestResults a -> [[a]]
 classes = sort . map sort . unsortedClasses
 
 unsortedClasses :: TestResults a -> [[a]]
-unsortedClasses (Results Nil) = []
-unsortedClasses (Results (NonNil t)) = aux t
+unsortedClasses (Results t) = aux t
   where aux Tree{rep = x, rest = xs, branches = []} = [x:xs]
         aux Tree{branches = bs} = concatMap aux bs
 
