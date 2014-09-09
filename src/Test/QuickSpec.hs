901a5c1bad7444506c62c0c67e4efbb449d63c3e
Nick Smallbone <nick.smallbone@gmail.com>
2014-09-09 21:13:05 +0200

    Add main file for testing.

---
+++ src/Test/QuickSpec.hs
@@ -0,0 +1,11 @@
+module Test.QuickSpec where
+
+import Test.QuickSpec.Base
+import Test.QuickSpec.Utils
+import Test.QuickSpec.Term
+import Test.QuickSpec.Type
+import Test.QuickSpec.Signature
+import Test.QuickSpec.TestTree
+import Test.QuickSpec.Eval
+import Test.QuickSpec.Equation
+import Test.QuickSpec.Pruning
