28fb1e5548e3fd4306f2c7e307261f79bb18f944
Nick Smallbone <nick.smallbone@gmail.com>
2014-09-15 17:16:40 +0200

    add t.hs

---
+++ src/t.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, DeriveDataTypeable, ScopedTypeVariables #-}
+import Data.Ratio
+import Test.QuickSpec
+import Test.QuickCheck
+import Control.Monad
+import Prelude hiding ((/), (\\))
+import qualified Prelude
+import Data.Typeable
+import Octonions
+import Test.QuickSpec.Eval
+import Test.QuickSpec.Signature hiding (sig)
+import qualified Test.QuickSpec.Signature as S
+import Data.Monoid
+
+(\\), (/) :: It -> It -> It
+a / b = a * recip b
+b \\ a = recip b * a
+
+l, r, l1, r1 :: It -> Fun
+l x = Fun (\y -> x * y)
+r x = Fun (\y -> y * x)
+l1 x = Fun (\y -> x \\ y)
+r1 x = Fun (\y -> y / x)
+
+sig = mconcat [
+  constant "1" (1 :: It),
+  constant "*" ((*) :: It -> It -> It),
+  constant "/" ((/) :: It -> It -> It),
+  constant "\\" ((\\) :: It -> It -> It),
+  ord (undefined :: It),
+  arb (undefined :: It)]
+
+main = quickSpec sig
+
+{-
+sig1 = [
+  withDepth 4,
+  withTests 10,
+  ["a", "b", "c"] `vars` (undefined :: It),
+  "1" `fun0` (1 :: It),
+  "*" `fun2` ((*) :: It -> It -> It),
+  "/" `fun2` ((/) :: It -> It -> It),
+  "\\" `fun2` ((\\) :: It -> It -> It)]
+
+sig2 = [
+  withSize 3,
+  withDepth 4,
+  withTests 10,
+  ["f", "g", "h"] `vars` (undefined :: Fun),
+  ["a", "b", "c"] `vars` (undefined :: It),
+  observer2 (\x (Fun f :: Fun) -> f x),
+  "*" `fun2` ((*) :: It -> It -> It),
+  "1" `fun0` (1 :: It),
+  "1" `blind0` (Fun (\x -> x)),
+  "." `blind2` (\(Fun f) (Fun g) -> Fun (\x -> f (g x))),
+  "l" `blind1` l,
+  "r" `blind1` r,
+  "l1" `blind1` l1,
+  "r1" `blind1` r1]
+-}
