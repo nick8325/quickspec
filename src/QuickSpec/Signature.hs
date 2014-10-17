10fbbe9a9584e61c5d21a3a79e337618fda68db3
Nick Smallbone <nick.smallbone@gmail.com>
2014-10-17 16:19:13 +0200

    Don't require CoArbitrary for baseType.

--- src/QuickSpec/Signature.hs
+++ src/QuickSpec/Signature.hs
@@ -177,19 +177,17 @@ isOp xs = not (all isIdent xs)
   where
     isIdent x = isAlphaNum x || x == '\'' || x == '_'
 
-baseType :: forall a. (Ord a, Arbitrary a, CoArbitrary a, Typeable a) => a -> [Instance]
+baseType :: forall a. (Ord a, Arbitrary a, Typeable a) => a -> [Instance]
 baseType _ =
   mconcat [
     inst (Sub Dict :: () :- Ord a),
-    inst (Sub Dict :: () :- Arbitrary a),
-    inst (Sub Dict :: () :- CoArbitrary a)]
+    inst (Sub Dict :: () :- Arbitrary a)]
 
-baseTypeNames :: forall a. (Ord a, Arbitrary a, CoArbitrary a, Typeable a) => [String] -> a -> [Instance]
+baseTypeNames :: forall a. (Ord a, Arbitrary a, Typeable a) => [String] -> a -> [Instance]
 baseTypeNames xs _ =
   mconcat [
     inst (Sub Dict :: () :- Ord a),
     inst (Sub Dict :: () :- Arbitrary a),
-    inst (Sub Dict :: () :- CoArbitrary a),
     names (NamesFor xs :: NamesFor a)]
 
 inst :: forall c1 c2. (Typeable c1, Typeable c2) => c1 :- c2 -> [Instance]
