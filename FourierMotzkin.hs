b34c967ce2906f015aefc67e1420589fa65deb02
Nick Smallbone <nick.smallbone@gmail.com>
2014-12-02 19:59:45 +0100

    Use a strict map (why not?)

--- FourierMotzkin.hs
+++ FourierMotzkin.hs
@@ -1,6 +1,6 @@
 import Data.Ratio
-import Data.Map(Map)
-import qualified Data.Map as Map
+import Data.Map.Strict(Map)
+import qualified Data.Map.Strict as Map
 import Data.Set(Set)
 import qualified Data.Set as Set
 import Data.Monoid
