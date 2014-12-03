0d0d3858683584f08caf1eb34b1426df86c27caa
Nick Smallbone <nick.smallbone@gmail.com>
2014-12-03 13:03:02 +0100

    Parametrise on the type of variables

--- FourierMotzkin.hs
+++ FourierMotzkin.hs
@@ -11,16 +11,13 @@ import Data.Ord
 import Control.Monad
 import Criterion.Main
 
-newtype Var = Var Char deriving (Eq, Ord, Enum)
-instance Show Var where show (Var x) = [x]
-
-data Term =
+data Term a =
   Term {
     constant :: Rational,
     -- Invariant: no coefficient is zero
-    vars :: Map Var Rational }
+    vars :: Map a Rational }
   deriving (Eq, Ord)
-instance Show Term where
+instance Show a => Show (Term a) where
   show (Term a vs)
     | Map.null vs = showRat a
     | a == 0 = showVars vs
@@ -32,19 +29,19 @@ showRat a
   | denominator a == 1 = show (numerator a)
   | otherwise = "(" ++ show a ++ ")"
 
-constTerm :: Rational -> Term
+constTerm :: Rational -> Term a
 constTerm a = Term a Map.empty
 
-var :: Var -> Term
+var :: a -> Term a
 var x = Term 0 (Map.singleton x 1)
 
-mapTerm :: (Rational -> Rational) -> Term -> Term
+mapTerm :: (Rational -> Rational) -> Term a -> Term a
 mapTerm f x =
   Term {
     constant = f (constant x),
     vars = fmap f (vars x) }
 
-instance Num Term where
+instance Ord a => Num (Term a) where
   fromInteger n = constTerm (fromInteger n)
   x + y =
     Term {
@@ -52,27 +49,27 @@ instance Num Term where
       vars = Map.filter (/= 0) (Map.unionWith (+) (vars x) (vars y)) }
   negate = mapTerm negate
 
-(^*) :: Rational -> Term -> Term
+(^*) :: Rational -> Term a -> Term a
 0 ^* y = constTerm 0
 x ^* y = mapTerm (x*) y
 
-eval :: Map Var Rational -> Term -> Rational
+eval :: Ord a => Map a Rational -> Term a -> Rational
 eval m t =
   constant t +
   sum [ a * Map.findWithDefault err x m | (x, a) <- Map.toList (vars t) ]
   where
     err = error "eval: variable not bound"
 
-data Problem =
+data Problem a =
   Unsolvable |
   Problem {
-    pos    :: Set Term,
-    lower  :: Map Var Rational,
-    upper  :: Map Var Rational,
-    pvars  :: Set Var }
+    pos    :: Set (Term a),
+    lower  :: Map a Rational,
+    upper  :: Map a Rational,
+    pvars  :: Set a }
   deriving (Eq, Ord)
 
-instance Show Problem where
+instance Show a => Show (Problem a) where
   show Unsolvable = "Unsolvable"
   show p =
     "[" ++ intercalate ", " xs ++ "]"
@@ -82,31 +79,31 @@ instance Show Problem where
         [show x ++ " >= " ++ showRat a | (x, a) <- Map.toList (lower p)] ++
         [show x ++ " <= " ++ showRat a | (x, a) <- Map.toList (upper p)]
 
-empty :: Problem
+empty :: Problem a
 empty = Problem Set.empty Map.empty Map.empty Set.empty
 
-problem :: [Term] -> Problem
+problem :: Ord a => [Term a] -> Problem a
 problem ts = addTerms ts (addVars ts empty)
 
 infix 4 ===, <==, >==
-(===), (<==), (>==) :: Term -> Term -> [Term]
+(===), (<==), (>==) :: Ord a => Term a -> Term a -> [Term a]
 t <== u = [u - t]
 t >== u = u <== t
 t === u = (t <== u) ++ (t >== u)
 
-addVars :: [Term] -> Problem -> Problem
+addVars :: Ord a => [Term a] -> Problem a -> Problem a
 addVars ts p =
   p { pvars = Set.union (Set.fromList vs) (pvars p) }
   where
     vs = concatMap (Map.keys . vars) ts
 
-addTerms :: [Term] -> Problem -> Problem
+addTerms :: Ord a => [Term a] -> Problem a -> Problem a
 addTerms _ Unsolvable = Unsolvable
 addTerms ts p = foldr addTerm (addBounds bs p) us
   where
     (bs, us) = partition ((== 1) . Map.size . vars) ts
 
-addTerm :: Term -> Problem -> Problem
+addTerm :: Ord a => Term a -> Problem a -> Problem a
 addTerm t p
   | Map.null (vars t) =
     if constant t < 0 then Unsolvable else p
@@ -114,7 +111,7 @@ addTerm t p
   | otherwise =
     p { pos = Set.insert t (Set.filter (not . implies p t) (pos p)) }
 
-addBounds :: [Term] -> Problem -> Problem
+addBounds :: Ord a => [Term a] -> Problem a -> Problem a
 addBounds [] p = p
 addBounds bs p =
   prune p { lower = Map.unionWith max (lower p) lower',
@@ -128,7 +125,7 @@ addBounds bs p =
         (x, a) = Map.findMin (vars t)
         b = negate (constant t) / a
 
-prune :: Problem -> Problem
+prune :: Ord a => Problem a -> Problem a
 prune p =
   p { pos = Set.filter (not . redundant p) (pos p) }
 
@@ -136,19 +133,19 @@ redundant p t =
   trivial p t ||
   or [ implies p u t && (t < u || not (implies p t u)) | u <- Set.toList (pos p), t /= u ]
 
-implies :: Problem -> Term -> Term -> Bool
+implies :: Ord a => Problem a -> Term a -> Term a -> Bool
 -- a1x1+...+anxn + b >= 0 ==> c1x1+...+cnxn + d >= 0
 -- <=>
 -- (c1-a1)x1+...+(cn-an)x2 + d - b >= 0
 implies p t u = trivial p (u - t)
 
-trivial :: Problem -> Term -> Bool
+trivial :: Ord a => Problem a -> Term a -> Bool
 trivial p t =
   case minValue p t of
     Just a | a >= 0 -> True
     _ -> False
 
-minValue :: Problem -> Term -> Maybe Rational
+minValue :: Ord a => Problem a -> Term a -> Maybe Rational
 minValue p t = do
   as <- fmap sum (mapM varValue (Map.toList (vars t)))
   return (as + constant t)
@@ -157,15 +154,15 @@ minValue p t = do
       | a > 0 = fmap (a *) (Map.lookup x (lower p))
       | otherwise = fmap (a *) (Map.lookup x (upper p))
 
-data Step = Stop | Eliminate Var [Term] [Term] Problem deriving Show
+data Step a = Stop | Eliminate a [Term a] [Term a] (Problem a) deriving Show
 
-eliminations :: Problem -> [Step]
+eliminations :: Ord a => Problem a -> [Step a]
 eliminations p =
   map snd .
   sortBy (comparing fst) $
     [ eliminate x p | x <- Set.toList (pvars p) ]
 
-eliminate :: Var -> Problem -> (Int, Step)
+eliminate :: Ord a => a -> Problem a -> (Int, Step a)
 eliminate x p =
   -- Number of terms added by the elimination
   (length ls * length us - length ls - length us,
@@ -183,7 +180,7 @@ eliminate x p =
     nontrivial (_:_:_) = True
     nontrivial _ = False
 
-focus :: Var -> Problem -> ([Term], [Term], Problem)
+focus :: Ord a => a -> Problem a -> ([Term a], [Term a], Problem a)
 focus x p = (ls', us', p' { pos = pos' })
   where
     p' = p {
@@ -208,7 +205,7 @@ foldDelete op e s = Set.foldr op' (e, s) s
         Nothing -> (y, s)
         Just y' -> (y', Set.delete x s)
 
-solve :: Problem -> Maybe (Map Var Rational)
+solve :: Ord a => Problem a -> Maybe (Map a Rational)
 solve Unsolvable = Nothing
 solve p | Set.null (pos p) = Just (Map.fromList xs)
   where
@@ -231,17 +228,17 @@ solveBounds :: (Maybe Rational, Maybe Rational) -> Rational
 solveBounds (x, y) = fromMaybe 0 (x `mplus` y)
 
 -- Debugging function
-trace :: Problem -> [Step]
+trace :: Ord a => Problem a -> [Step a]
 trace Unsolvable = []
 trace p | Set.null (pos p) = []
 trace p = s:trace p'
   where
     s@(Eliminate _ _ _ p'):_ = eliminations p
 
-x = var (Var 'x')
-y = var (Var 'y')
-z = var (Var 'z')
-w = var (Var 'w')
+x = var 'x'
+y = var 'y'
+z = var 'z'
+w = var 'w'
 
 cs0 =
   concat [
