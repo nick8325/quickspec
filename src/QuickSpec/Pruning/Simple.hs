7f8f3a350bc2e3517afb03851c440d4a1c79a4da
Nick Smallbone <nick.smallbone@gmail.com>
2014-12-15 13:35:51 +0100

    Make the simple pruner work again.

--- src/QuickSpec/Pruning/Simple.hs
+++ src/QuickSpec/Pruning/Simple.hs
@@ -4,66 +4,36 @@ import QuickSpec.Base
 import QuickSpec.Prop
 import QuickSpec.Pruning
 import QuickSpec.Term
+import QuickSpec.Utils
 import Control.Monad
 import Control.Monad.Trans.State.Strict
+import Data.Ord
+import Data.Maybe
+import Data.Rewriting.Rule
+import qualified QuickSpec.Pruning.Index as Index
+import QuickSpec.Pruning.Index (Index)
+import QuickSpec.Pruning.Rewrite
 
-newtype SimplePruner = S [PropOf PruningTerm]
+newtype SimplePruner = S (Index (RuleOf PruningTerm))
 
 instance Pruner SimplePruner where
-  emptyPruner _ = S []
+  emptyPruner _ = S Index.empty
   untypedAxiom  = simpleUnify
   untypedRep    = simpleRep
 
 modifyS f = modify (\(S x) -> S (f x))
 
-simpleAreEqual :: Monad m => PropOf PruningTerm -> StateT SimplePruner m (Maybe PruningTerm)
-simpleAreEqual (lhs :=>: t :=: u)
-  | measure (fromPruningTerm t) <= measure (fromPruningTerm u) = do
-    S eqs <- get
-    return (simplifies (map unitProp lhs ++ eqs) u)
-  | otherwise =
-    simpleAreEqual (lhs :=>: u :=: t)
-simpleAreEqual _ = return Nothing
-
 simpleUnify :: Monad m => PropOf PruningTerm -> StateT SimplePruner m ()
-simpleUnify prop@(lhs :=>: t :=: u) = do
-  res <- simpleAreEqual prop
-  case res of
-    Just v |
-      fromPruningTerm v `alwaysSimplerThan` fromPruningTerm t ||
-      fromPruningTerm v `alwaysSimplerThan` fromPruningTerm u ->
-        return ()
-    _ ->
-      modifyS (prop:)
-simpleUnify prop = modifyS (prop:)
-
-alwaysSimplerThan :: Term -> Term -> Bool
-t `alwaysSimplerThan` u =
-  size t <= size u &&
-  measure (schema t) < measure (schema u) &&
-  and [ sizeOk v | v <- vars t, v `elem` vars u ]
-  where
-    sizeOk v = occ v t <= occ v u
-    occ v t = length [ v' | v' <- vars t, v == v' ]
+simpleUnify prop@([] :=>: t :=: u) = modifyS (Index.insert (Rule t u) . Index.insert (Rule u t))
+simpleUnify _ = return ()
 
 simpleRep :: Monad m => [PropOf PruningTerm] -> PruningTerm -> StateT SimplePruner m (Maybe PruningTerm)
 simpleRep axioms t = do
-  S eqs <- get
-  return (simplifies (axioms++eqs) t)
-
-simplifies :: [PropOf PruningTerm] -> PruningTerm -> Maybe PruningTerm
-simplifies eqs t =
-  msum [ simplifies1 u v t `mplus` simplifies1 v u t | [] :=>: u :=: v <- eqs ] `mplus`
-  simplifiesThere eqs t
-simplifiesThere eqs (Fun f ts) =
-  fmap (Fun f) (simplifiesList eqs ts)
-simplifiesThere _ _ = Nothing
-simplifiesList eqs [] = Nothing
-simplifiesList eqs (t:ts) = fmap (:ts) (simplifies eqs t) `mplus` fmap (t:) (simplifiesList eqs ts)
-
-simplifies1 :: PruningTerm -> PruningTerm -> PruningTerm -> Maybe PruningTerm
-simplifies1 t u v = do
-  s <- match t v
-  let w = subst s u
-  guard (measure (fromPruningTerm w) < measure (fromPruningTerm v))
-  return w
+  S idx <- get
+  let u = normaliseWith (usortBy (comparing measure) . anywhere (rewrite idx)) t
+  if t == u then return Nothing else return (Just u)
+  where
+    rewrite idx t = do
+      Rule _ u <- Index.lookup t idx
+      guard (measure u < measure t)
+      return u
