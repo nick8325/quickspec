2ca04a90c50800fd61b24f0d4bfb52fe7fcdf5b2
Nick Smallbone <nick.smallbone@gmail.com>
2015-02-26 20:15:52 +0100

    Fix mucked-up move

---
+++ examples/PrettyPrinting.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE DeriveDataTypeable, TypeOperators, StandaloneDeriving #-}
+import Control.Monad
+import Test.QuickCheck
+import QuickSpec hiding (background, (<>), text, nest, ($$))
+import Text.PrettyPrint.HughesPJ
+
+deriving instance Typeable Doc
+
+instance Arbitrary Doc where
+  arbitrary =
+    sized $ \n ->
+      let bin = resize (n `div` 2) arbitrary
+          un = resize (n-1) arbitrary in
+      oneof $
+        [ liftM2 ($$) bin bin | n > 0 ] ++
+        [ liftM2 (<>) bin bin | n > 0 ] ++
+        [ liftM2 nest arbitrary un | n > 0 ] ++
+        [ fmap text arbString ]
+
+arbString :: Gen String
+arbString = listOf (elements "ab")
+
+background =
+  signature {
+    maxTests = Just 1000,
+    constants = [
+       constant "[]" ([] :: [A]),
+       constant "++" ((++) :: [A] -> [A] -> [A]),
+       constant "0" (0 :: Int),
+       constant "+" ((+) :: Int -> Int -> Int),
+       constant "length" (length :: String -> Int) ]}
+
+-- obsDoc :: Doc -> Gen String
+-- obsDoc d = do
+--   n <- arbitrary
+--   return (render (nest n d))
+
+obsDoc :: Doc -> Gen String
+obsDoc d = fmap render ctx
+  where
+    ctx =
+      sized $ \n ->
+      oneof $
+        [ return d ] ++
+        [ liftM2 op (resize (n `div` 2) ctx) (resize (n `div` 2) arbitrary) | n > 0, op <- [(<>), ($$)] ] ++
+        [ liftM2 op (resize (n `div` 2) arbitrary) (resize (n `div` 2) ctx) | n > 0, op <- [(<>), ($$)] ] ++
+        [ liftM2 nest arbitrary (resize (n-1) ctx) | n > 0 ]
+
+unindented :: Doc -> Bool
+unindented d = render (nest 100 (text "" <> d)) == render (nest 100 d)
+
+nesting :: Doc -> Int
+nesting d = head [ i | i <- nums, unindented (nest (-i) d) ]
+  where
+    nums = 0:concat [ [i, -i] | i <- [1..] ]
+
+sig =
+  signature {
+    maxTests = Just 1000,
+    constants = [
+       constant "text" text,
+       constant "nest" nest,
+       --constant "nesting" nesting,
+       constant "<>" (<>),
+       constant "$$" ($$) ],
+    instances = [
+      makeInstance (\() -> arbString),
+      makeInstance (\() -> observe obsDoc),
+      inst (Sub Dict :: () :- Arbitrary Doc) ],
+    defaultTo = Just (typeOf (undefined :: Bool)) }
+
+main = quickSpecWithBackground background sig
--- examples/PrettyPrintingModel.hs
+++ examples/PrettyPrintingModel.hs
@@ -1,75 +1,49 @@
-{-# LANGUAGE DeriveDataTypeable, TypeOperators, StandaloneDeriving #-}
+{-# LANGUAGE DeriveDataTypeable, TypeOperators #-}
 import Control.Monad
 import Test.QuickCheck
 import QuickSpec hiding (background, (<>), text, nest, ($$))
-import Text.PrettyPrint.HughesPJ
 
-deriving instance Typeable Doc
+newtype Layout a = Layout [(Int, [a])] deriving (Typeable, Eq, Ord, Show)
 
-instance Arbitrary Doc where
-  arbitrary =
-    sized $ \n ->
-      let bin = resize (n `div` 2) arbitrary
-          un = resize (n-1) arbitrary in
-      oneof $
-        [ liftM2 ($$) bin bin | n > 0 ] ++
-        [ liftM2 (<>) bin bin | n > 0 ] ++
-        [ liftM2 nest arbitrary un | n > 0 ] ++
-        [ fmap text arbString ]
+instance Arbitrary a => Arbitrary (Layout a) where
+  arbitrary = fmap Layout (liftM2 (:) arbitrary arbitrary)
 
-arbString :: Gen String
-arbString = listOf (elements "ab")
+text :: [a] -> Layout a
+text s = Layout [(0, s)]
+
+nest :: Int -> Layout a -> Layout a
+nest k (Layout l) = Layout [(i+k, s) | (i, s) <- l]
+
+($$) :: Layout a -> Layout a -> Layout a
+Layout xs $$ Layout ys = Layout (xs ++ ys)
+
+(<>) :: Layout a -> Layout a -> Layout a
+Layout xs <> Layout ys = f (init xs) (last xs) (head ys) (tail ys)
+  where f xs (i, s) (j, t) ys = Layout xs $$ Layout [(i, s ++ t)] $$ nest (i + length s - j) (Layout ys)
+
+nesting :: Layout a -> Int
+nesting (Layout ((i,_):_)) = i
 
 background =
   signature {
-    maxTests = Just 1000,
     constants = [
        constant "[]" ([] :: [A]),
        constant "++" ((++) :: [A] -> [A] -> [A]),
        constant "0" (0 :: Int),
        constant "+" ((+) :: Int -> Int -> Int),
-       -- FIXME why does this not work when we use [A]?
-       -- Answer: because in the schema, text _ and length _
-       -- have different hole types.
-       constant "length" (length :: [Char] -> Int) ]}
-
--- obsDoc :: Doc -> Gen String
--- obsDoc d = do
---   n <- arbitrary
---   return (render (nest n d))
-
-obsDoc :: Doc -> Gen String
-obsDoc d = fmap render ctx
-  where
-    ctx =
-      sized $ \n ->
-      oneof $
-        [ return d ] ++
-        [ liftM2 op (resize (n `div` 2) ctx) (resize (n `div` 2) arbitrary) | n > 0, op <- [(<>), ($$)] ] ++
-        [ liftM2 op (resize (n `div` 2) arbitrary) (resize (n `div` 2) ctx) | n > 0, op <- [(<>), ($$)] ] ++
-        [ liftM2 nest arbitrary (resize (n-1) ctx) | n > 0 ]
-
-unindented :: Doc -> Bool
-unindented d = render (nest 100 (text "" <> d)) == render (nest 100 d)
-
-nesting :: Doc -> Int
-nesting d = head [ i | i <- nums, unindented (nest (-i) d) ]
-  where
-    nums = 0:concat [ [i, -i] | i <- [1..] ]
+       constant "length" (length :: [A] -> Int) ]}
 
 sig =
   signature {
-    maxTests = Just 1000,
     constants = [
-       constant "text" text,
-       constant "nest" nest,
-       --constant "nesting" nesting,
-       constant "<>" (<>),
-       constant "$$" ($$) ],
+       constant "text" (text :: [A] -> Layout A),
+       constant "nest" (nest :: Int -> Layout A -> Layout A),
+       constant "nesting" (nesting :: Layout A -> Int),
+       constant "$$" (($$) :: Layout A -> Layout A -> Layout A),
+       constant "<>" ((<>) :: Layout A -> Layout A -> Layout A) ],
     instances = [
-      makeInstance (\() -> arbString),
-      makeInstance (\() -> observe obsDoc),
-      inst (Sub Dict :: () :- Arbitrary Doc) ],
+      inst (Sub Dict :: Ord A         :- Ord       (Layout A)),
+      inst (Sub Dict :: Arbitrary A   :- Arbitrary (Layout A)) ],
     defaultTo = Just (typeOf (undefined :: Bool)) }
 
 main = quickSpecWithBackground background sig
