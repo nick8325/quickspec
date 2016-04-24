9737ae0bab2ea172b29d3f23b4131d6724da4efb
Nick Smallbone <nick.smallbone@gmail.com>
2016-04-24 23:42:42 +0200

    stuff

--- src/QuickSpec/Term.hs
+++ src/QuickSpec/Term.hs
@@ -1,3 +1,10 @@
+-- New plan:
+-- simple type of terms like in pre-twee QS.
+-- Parametrised on constants and variables.
+-- On second thoughts, just introduce term type first.
+-- Then reintroduce schemas, then proper parametrisation
+-- (and maybe not just on constant type - on term type).
+
 -- Typed terms.
 {-# LANGUAGE RecordWildCards, PatternSynonyms, CPP #-}
 module QuickSpec.Term where
@@ -10,24 +17,35 @@ import qualified Twee.Base as Twee
 import Twee.Base hiding (Term)
 import Control.Monad
 
+-- Twee terms are untyped.
+-- So we pair a Twee term with a context, a map from variable to type.
 data Term f =
   Term {
-    term    :: Twee.Term f,
-    context :: IntMap Type }
+    tm_term :: Twee.Term f,
+    tm_ctx  :: IntMap Type }
 
 instance (Typed f, Numbered f) => Typed (Term f) where
   typ Term{..} =
-    case term of
+    case tm_term of
       Var (MkVar x) ->
-        Map.findWithDefault __ x context
-      -- the type of a constant should assume it's been applied
+        Map.findWithDefault __ x tm_ctx
       App f _ -> typ f
 
   otherTypesDL Term{..} =
-    msum [return (typ (Term t context)) | t <- properSubterms term] `mplus`
-    msum (map return (Map.elems context))
+    msum [return (typ (Term t tm_ctx)) | t <- properSubterms tm_term] `mplus`
+    msum (map return (Map.elems tm_ctx))
 
   typeReplace sub Term{..} =
     Term {
-      term    = build (mapFun (toFun . typeReplace sub . fromFun) term),
-      context = fmap (typeReplace sub) context }
+      term    = build (mapFun (toFun . typeReplace sub . fromFun) tm_term),
+      context = fmap (typeReplace sub) tm_ctx }
+
+instance Symbolic (Term f) where
+  type ConstantOf (Term f) = f
+  term Term{..} = tm_term
+  termsDL = return . tm_term
+  replace sub (Term t ctx) = Term (replace sub t) ctx
+  -- XXXXX oh no!
+  -- substitution doesn't respect types!
+  -- Need our own QuickSpec-symbolic class.
+  -- Or just have our own typed-term type without twee.
