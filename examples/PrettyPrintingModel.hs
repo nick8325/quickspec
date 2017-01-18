39b6208213331a92deed7d0c31d6fdde124d7012
Nick Smallbone <nick.smallbone@gmail.com>
2017-01-18 17:33:09 +0100

    Make pretty-printing example monomorphic (it's more natural)

--- examples/PrettyPrintingModel.hs
+++ examples/PrettyPrintingModel.hs
@@ -3,48 +3,58 @@ import Control.Monad
 import Test.QuickCheck
 import QuickSpec hiding (background, (<>), text, nest, ($$))
 
-newtype Layout a = Layout [(Int, [a])] deriving (Typeable, Eq, Ord, Show)
+newtype Layout = Layout [(Int, String)]
+  deriving (Typeable, Eq, Ord, Show)
 
-instance Arbitrary a => Arbitrary (Layout a) where
+instance Arbitrary Layout where
   arbitrary = fmap Layout (liftM2 (:) arbitrary arbitrary)
 
-text :: [a] -> Layout a
+text :: String -> Layout
 text s = Layout [(0, s)]
 
-nest :: Int -> Layout a -> Layout a
+nest :: Int -> Layout -> Layout
 nest k (Layout l) = Layout [(i+k, s) | (i, s) <- l]
 
-($$) :: Layout a -> Layout a -> Layout a
+($$) :: Layout -> Layout -> Layout
 Layout xs $$ Layout ys = Layout (xs ++ ys)
 
-(<>) :: Layout a -> Layout a -> Layout a
-Layout xs <> Layout ys = f (init xs) (last xs) (head ys) (tail ys)
-  where f xs (i, s) (j, t) ys = Layout xs $$ Layout [(i, s ++ t)] $$ nest (i + length s - j) (Layout ys)
+(<>) :: Layout -> Layout -> Layout
+Layout xs <> Layout ys =
+  combine (init xs) (last xs) (head ys) (tail ys)
+  where
+    combine xs (i, s) (j, t) ys =
+      Layout xs $$
+      Layout [(i, s ++ t)] $$
+      nest (i + length s - j) (Layout ys)
 
-nesting :: Layout a -> Int
+nesting :: Layout -> Int
 nesting (Layout ((i,_):_)) = i
 
 background =
   signature {
     maxTermSize = Just 9,
     constants = [
-       constant "[]" ([] :: [A]),
-       constant "++" ((++) :: [A] -> [A] -> [A]),
-       constant "0" (0 :: Int),
-       constant "+" ((+) :: Int -> Int -> Int),
-       constant "length" (length :: [A] -> Int) ]}
+      constant "\"\"" ("" :: String),
+      constant "++" ((++) :: String -> String -> String),
+      constant "0" (0 :: Int),
+      constant "+" ((+) :: Int -> Int -> Int),
+      constant "length" (length :: String -> Int) ]}
 
 sig =
   signature {
     constants = [
-       constant "text" (text :: [A] -> Layout A),
-       constant "nest" (nest :: Int -> Layout A -> Layout A),
-       constant "nesting" (nesting :: Layout A -> Int),
-       constant "$$" (($$) :: Layout A -> Layout A -> Layout A),
-       constant "<>" ((<>) :: Layout A -> Layout A -> Layout A) ],
-    instances = [
-      inst (Sub Dict :: Ord A         :- Ord       (Layout A)),
-      inst (Sub Dict :: Arbitrary A   :- Arbitrary (Layout A)) ],
-    defaultTo = Just (typeOf (undefined :: Bool)) }
+      constant "text" (text :: String -> Layout),
+      constant "nest" (nest :: Int -> Layout -> Layout),
+      constant "$$" (($$) :: Layout -> Layout -> Layout),
+      constant "<>" ((<>) :: Layout -> Layout -> Layout) ],
+    instances = [baseType (undefined :: Layout) ]}
 
-main = quickSpecWithBackground background sig
+sig' =
+  signature {
+    constants = [
+      constant "nesting" (nesting :: Layout -> Int) ]}
+
+main = do
+  backgroundThy <- quickSpec background
+  thy <- quickSpec (backgroundThy `mappend` sig)
+  quickSpec (thy `mappend` sig')
