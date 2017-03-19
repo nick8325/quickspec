0efb7a724ae97a595bae66f8321e49e30a9d4b06
Maximilian Algehed <m.algehed@gmail.com>
2017-03-19 18:02:39 +0100

    Fixed a bug with the interface

--- examples/Conditionals.hs
+++ examples/Conditionals.hs
@@ -1,30 +1,17 @@
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
-import Control.Monad
-import Test.QuickCheck
 import QuickSpec
-import Data.Dynamic
 import GHC.TypeLits
-import QuickSpec.Signature
 
 sig =
   signature {
     maxTermSize = Just 8,
-    --maxTests    = Just 10000,
     constants = [
-       --constant "[]" ([] :: [Int]),
-       --constant ":"  ((:) :: Int -> [Int] -> [Int]),
-       constant "++" ((++) :: [A] -> [A] -> [A]),
-       --constant "head" (head :: [A] -> A),
-       constant "zip" (zip :: [A] -> [A] -> [(A,A)])
-       --constant "length" (length :: [A] -> Int),
-       --constant "reverse" (reverse :: [A] -> [A])
+       constant "++" ((++) :: [Int] -> [Int] -> [Int]),
+       constant "zip" (zip :: [Int] -> [Int] -> [(Int,Int)])
     ],
-    predicates = [--predicate (undefined :: Proxy "notNull") ((not . null) :: [Int] -> Bool),
-                  predicate (undefined :: Proxy "eqLen")
-                  ((\xs ys -> length xs == length ys) :: [A] -> [A] -> Bool)]
+    predicates = [ predicate (undefined :: Proxy "eqLen")
+                  ((\xs ys -> length xs == length ys) :: [Int] -> [Int] -> Bool)
+                 ]
    }
 
-main = quickSpec sig --mapM_ print (instances_ (predicateSig sig))
-  
---quickSpec sig
+main = quickSpec sig
