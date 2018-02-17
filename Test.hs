d5168f9f7e2459ddc27239d20c6964ca51a4536b
Nick Smallbone <nick@smallbone.se>
2018-02-17 22:28:31 +0100

    Remove old examples

--- Test.hs
+++
@@ -1,8 +0,0 @@
-import Data.Proxy
-import QuickSpec
-
-main = quickSpec [
-  constant "+" ((+) :: Int -> Int -> Int),
-  constant "*" ((*) :: Int -> Int -> Int),
-  constant "0" (0 :: Int),
-  constant "1" (1 :: Int)]
