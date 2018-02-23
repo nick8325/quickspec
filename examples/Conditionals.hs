5f731f4763bf10a39bcda3ec9b9c8ae787aff7a9
Nick Smallbone <nick@smallbone.se>
2018-02-23 12:00:36 +0100

    snapshot

--- examples/Conditionals.hs
+++ examples/Conditionals.hs
@@ -8,6 +8,7 @@ eqLen :: [a] -> [b] -> Bool
 eqLen xs ys = length xs == length ys
 
 main = quickSpec [
+  -- Explore bigger terms.
   withMaxTermSize 8,
   withPruningDepth 10,
   con "++" ((++) @ Int),
