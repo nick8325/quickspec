260bd06e07f0c8667bca4e592cdac117aeae23bf
Ashwin Mathi <ashwinmathi@gmail.com>
2022-10-06 02:32:24 -0400

    fixed accidental re-formatting

--- src/QuickSpec/Internal/Pruning.hs
+++ src/QuickSpec/Internal/Pruning.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, GeneralizedNewtypeDeriving, FlexibleInstances, UndecidableInstances, DefaultSignatures, GADTs #-}
 module QuickSpec.Internal.Pruning where
 
-
 import QuickSpec.Internal.Type (Arity (..))  
 import QuickSpec.Internal.Prop
 import QuickSpec.Internal.Testing
